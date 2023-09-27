42b86e8de6ac7c89cf69e1edda865f47c5401893
Michał Duk <mrrokiton1@gmail.com>
2023-09-27 16:36:03 +0200

    Add styles for not found page

--- src/general/not-found/not-found.tsx
+++ src/general/not-found/not-found.tsx
@@ -1,11 +1,17 @@
 import { RoutePaths } from '../route-paths.js';
 import { Link } from 'react-router-dom';
 
+import './not-found.scss';
+
 export const NotFound = () => {
 	return (
-		<div>
-			<h3>Page not found.</h3>
-			<Link to={RoutePaths.HOME}>Go Home</Link>
+		<div className='not-found'>
+			<div className='not-found-container'>
+				<h3>Page not found.</h3>
+				<Link to={RoutePaths.HOME} className='not-found-link'>
+					Go Home
+				</Link>
+			</div>
 		</div>
 	);
 };
